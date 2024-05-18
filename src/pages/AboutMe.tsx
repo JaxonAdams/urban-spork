f6bceb16866a175f3888c72499c64eff064c07b6
Jaxon Adams <jaxon.adams@loanpro.io>
2024-05-18 12:04:10 -0600

    added some info on my about me page

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -3,7 +3,20 @@ import React from "react";
 const AboutMe: React.FC = () => {
     return (
         <div className="about-me">
-            <h1>Coming Soon!</h1>
+            <div className="about-me-txt-container">
+                <h1 className="txt-container-header">Always Learning</h1>
+                <p className="txt">
+                    I'm a software engineer located in Eagle Mountain, Utah.
+                    I tend to prefer writing server side code, but I have many
+                    front-end skills as well.
+                </p>
+                <p className="txt">
+                    I'm always working to improve as a developer, and I love picking
+                    up new skills. I'm drawn to the challenges inherent in software
+                    development, and find designing and building software extremely
+                    rewarding.
+                </p>
+            </div>
         </div>
     );
 }
