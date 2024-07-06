5a6f4f73a0d79e4d2e1eeab8071fef1911487488
Jaxon Adams <jadamsresume1@gmail.com>
2024-07-05 22:17:57 -0600

    updated image loading process

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -14,15 +14,16 @@ interface Project {
 
 interface Props {
     project: Project;
+    imageSrc: string;
 }
 
-const ProjectCard: React.FC<Props> = ({ project }) => {
+const ProjectCard: React.FC<Props> = ({ project, imageSrc }) => {
     return (
         <div className="project-card">
             <div className="project-card-left">
                 <h3 className="project-title">{project.projectTitle}</h3>
                 <div className="project-img-container">
-                    <img className="project-img" src={project.image} alt={project.projectTitle} />
+                    <img className="project-img" src={imageSrc} alt={project.projectTitle} />
                 </div>
             </div>
             <div className="project-card-right">
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 
-import projectsData from "../assets/projects/projects.json";
+import projectsData from "../assets/projects.json";
 import ProjectCard from "../components/ProjectCard/ProjectCard";
+
+import wgups from "../assets/projects/wgups_img.png";
+import pyrpg from "../assets/projects/pyrpg_img.jfif";
+import tinyurl from "../assets/projects/tinyurl_img.png";
+
+interface ImageMap {
+    [key: string]: string;
+}
+
+const images: ImageMap = {
+    "WGUPS": wgups,
+    "PyGame RPG": pyrpg,
+    "URL Shortener": tinyurl
+};
 
 const Projects: React.FC = () => {
 
     return (
         <div className="projects">
             {projectsData.map(project => {
-                return <ProjectCard project={project} key={project.projectTitle} />;
+                return <ProjectCard project={project} key={project.projectTitle} 
+                                    imageSrc={images[project.projectTitle]} />;
             })}
         </div>
     );
