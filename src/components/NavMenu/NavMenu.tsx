ce250e6f7f594081fb35a3e9e62fc4b73a73bbe5
Jaxon Adams <jadamsresume1@gmail.com>
2025-02-28 16:56:51 -0700

    Fix bug where menu is navigable when hidden

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -12,7 +12,11 @@ const NavMenu: React.FC<Props> = ({ visible, setPage }) => {
         return currentPageStr == pathname;
     }
 
-    const updateCurPage = (pageStr: string) => {
+    const updateCurPage = (pageStr: string, isClickable: boolean) => {
+        if (!isClickable) {
+            return;
+        }
+        
         setCurrentPageStr(pageStr);
         setPage(pageStr);
     };
@@ -21,15 +25,15 @@ const NavMenu: React.FC<Props> = ({ visible, setPage }) => {
         <nav className={`nav-menu ${visible && "nav-visible"}`}>
             <ul>
                 <li className={`menu-item txt ${currentlyOnPage("home") && "txt-highlight"}`}
-                    onClick={() => {updateCurPage("home")}}>
+                    onClick={() => {updateCurPage("home", visible)}}>
                     Home
                 </li>
                 <li className={`menu-item txt ${currentlyOnPage("aboutMe") && "txt-highlight"}`}
-                    onClick={() => {updateCurPage("aboutMe")}}>
+                    onClick={() => {updateCurPage("aboutMe", visible)}}>
                     About Me
                 </li>
                 <li className={`menu-item txt ${currentlyOnPage("projects") && "txt-highlight"}`}
-                    onClick={() => {updateCurPage("projects")}}>
+                    onClick={() => {updateCurPage("projects", visible)}}>
                     Projects
                 </li>
             </ul>
