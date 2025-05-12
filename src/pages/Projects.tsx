c73126e6680c9c81112154a3b6a08c9d966cc97f
Jaxon Adams <jadamsresume1@gmail.com>
2025-05-12 12:08:18 -0600

    Add WGU capstone project to projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import projectsData from "../assets/projects.json";
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 
+import wguCapstone from "../assets/projects/capstone.png";
 import premiumCalculator from "../assets/projects/premium_calculator_img.png";
 import wgups from "../assets/projects/wgups_img.png";
 import pyrpg from "../assets/projects/pyrpg_img.jfif";
@@ -14,6 +15,7 @@ interface ImageMap {
 }
 
 const images: ImageMap = {
+    "WGU Capstone - ML Loan Risk Assessment": wguCapstone,
     "Premium Calculator": premiumCalculator,
     "WGUPS": wgups,
     "PyGame RPG": pyrpg,
