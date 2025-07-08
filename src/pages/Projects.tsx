0c86513c608316d791f4e9633cb10db5fa054295
Jaxon Adams <jadamsresume1@gmail.com>
2025-07-08 16:10:21 -0600

    Add fake news classifier project

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import projectsData from "../assets/projects.json";
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 
+import fakeNews from "../assets/projects/fake_news_img.png";
 import wguCapstone from "../assets/projects/capstone.png";
 import premiumCalculator from "../assets/projects/premium_calculator_img.png";
 import pyrpg from "../assets/projects/pyrpg_img.jfif";
@@ -13,6 +14,7 @@ interface ImageMap {
 }
 
 const images: ImageMap = {
+    "Fake News Classifier": fakeNews,
     "WGU Capstone - ML Loan Risk Assessment": wguCapstone,
     "Premium Calculator": premiumCalculator,
     "PyGame RPG": pyrpg,
@@ -30,7 +32,7 @@ const Projects: React.FC = () => {
             <h1 className="txt-container-header">Recent Projects</h1>
             <div className="projects">
                 {projectsData.map(project => {
-                    return <ProjectCard project={project} key={project.projectTitle} 
+                    return <ProjectCard project={project} key={project.projectTitle}
                         imageSrc={images[project.projectTitle]} />;
                 })}
             </div>
